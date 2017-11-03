0ebee079f52d3d5a2f90763519b9e9fba9b18002
majingga02 <majingga02@naver.com>
2017-11-03 13:20:48 +0900

    재 없뎃

---
+++ Assets/_Origin/Map/Script/Editor/MapEditor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(MapGenerator))] // CustomEditor 사용할 클래스 선언 
+[CanEditMultipleObjects]
+public class MapEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        MapGenerator map = target as MapGenerator;
+
+        if (DrawDefaultInspector())
+        {
+            
+
+
+        }
+
+        if (GUILayout.Button("TILE LIST RESET"))
+        {
+            map.tilelistReset();
+        }
+
+        if (GUILayout.Button("Generate Map"))
+        {
+            map.GeneratorMap();
+        }
+
+        if (GUILayout.Button("AllTileUpdate"))
+        {
+            //map.Update();
+            map.reloadTile();
+        }
+
+        if (GUILayout.Button("ChangeTileSize"))
+        {
+            map.TileSizeChange();
+        }
+
+        if(GUILayout.Button("DicLoad"))
+        {
+            map.DicLoad();
+        }
+    }
+}
+
+[CustomEditor(typeof(Tile))]
+[CanEditMultipleObjects]
+public class TileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        
+        Tile tile = target as Tile;
+        base.OnInspectorGUI();
+
+        if (DrawDefaultInspector())
+        {
+            tile.tileUpdate();
+
+
+        }
+        
+
+
+
+        if (GUILayout.Button("Update"))
+        {
+            tile.tileUpdate();
+        }
+
+        if(GUILayout.Button("STACK"))
+        {
+            tile.StackTile();
+        }
+
+        if(GUILayout.Button("DeleteStack"))
+        {
+            tile.DeleteStackParent();
+        }
+    }
+}
---
+++ Assets/_Origin/Map/Script/Test_Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Player : MonoBehaviour
+{
+
+    public float speed = 0;
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed;
+        }
+            if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.forward * speed;
+        }
+                if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right * speed;
+        }
+                    if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed;
+        }
+
+                    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += Vector3.up * 5;
+        }
+    }
+
+}
